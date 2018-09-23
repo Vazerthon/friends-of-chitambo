6a00a6b6a3f87b05fe30238354f31cd6a8fa6abe
Paul Vallis <vallis1@gmail.com>
2018-09-23 15:57:25 +0100

    Add gh-pages. Remove build output

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/home/vallis/Projects/friends-of-chitambo/node_modules/gatsby-plugin-offline/gatsby-browser'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/home/vallis/Projects/friends-of-chitambo/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
