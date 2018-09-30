c95166a098c20ff087ecd7908fc43d58c0fb7758
Paul Vallis <vallis1@gmail.com>
2018-09-30 17:12:55 +0100

    Adjust container padding/margin

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Helmet from 'react-helmet';
+
 import Root from '../Root';
 
 const Container = styled.div`
   padding: ${({ theme }) => theme.spacing.units(2)};
   padding-bottom: 0;
+  margin: 0 ${({ theme }) => theme.spacing.units(20)};
+
+  ${({ theme }) => theme.media.medium`
+    margin: 0 ${theme.spacing.units(10)};
+  `}
 
   ${({ theme }) => theme.media.small`
     padding: ${theme.spacing.units(1)};
+    padding-bottom: 0;
+    margin: 0;
   `}
 `;
 
