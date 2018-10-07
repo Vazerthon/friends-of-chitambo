2289e3a43a6cf795c967587e42fbc02d0a18fe53
Paul Vallis <vallis1@gmail.com>
2018-10-07 13:51:02 +0100

    Use slick

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,97 +1,31 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React from 'react';
+import Slider from 'react-slick';
 import PropTypes from 'prop-types';
 
-const SlideContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-  overflow: hidden;
-`;
+const sliderSettings = {
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
 
-const Slide = styled.div`
-  ${({ prev }) =>
-    prev && `
-    & {
-      transform: translateX(-100%);
-    }`
-  }
-  
-  ${({ curr }) =>
-    curr && `
-    & {
-      transform: translateX(0);
-      opacity: 1;
-    }`
-  }
-
-  opacity: 0;
-  display: flex;
-  position: absolute;
-  transform: translateX(100%);
-
-  transition: all 0.5s ease-in-out;
-
-  justify-content: center;
-  align-items: center;
-`;
-
-class Carousel extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      prevIndex: 0,
-      index: 1,
-      nextIndex: 2,
-    };
-
-    this.imageInterval = setInterval(() => this.tickImage(), 5000);
-  }
-
-  tickImage() {
-    const { images } = this.props;
-    const { index } = this.state;
-    const nextIndex = index + 1 === images.length ? 0 : index + 1;
-    const nextNextIndex = nextIndex + 1 === images.length ? 0 : nextIndex + 1;
-
-    const newState = {
-      prevIndex: index,
-      index: nextIndex,
-      nextIndex: nextNextIndex,
-    };
-
-    this.setState(newState);
-  }
-
-  imageInterval = undefined;
-
-  render() {
-    const { images } = this.props;
-    const { prevIndex, index, nextIndex } = this.state;
-
-    return (
-      <SlideContainer>
-        {images.map((image, i) => (
-          <Slide
-            key={image.id}
-            prev={i === prevIndex}
-            curr={i === index}
-          >
-            <img
-              src={image.src}
-              alt={image.alt}
-            />
-          </Slide>
-        ))}
-      </SlideContainer>
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.imageInterval);
-  }
+export default function Carousel({ images }) {
+  return (
+    <Slider {...sliderSettings}>
+      {images.map(({ src, alt, id }) => (
+        <img
+          key={id}
+          src={src}
+          alt={alt}
+        />
+      ))}
+    </Slider>
+  );
 }
 
 Carousel.propTypes = {
@@ -103,5 +37,3 @@ Carousel.propTypes = {
     }),
   ).isRequired,
 };
-
-export default Carousel;
