aa107d7176415b128ac17b3e21aa34bdd7ed16c6
Paul Vallis <vallis1@gmail.com>
2018-10-14 17:46:25 +0100

    Move drawer menu into layout

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,38 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Helmet from 'react-helmet';
 
-import Root from '../Root';
+import Toolbar from '@material-ui/core/Toolbar';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import DrawerMenu from './drawer-menu';
+
+import Root from './root';
 
 const Container = styled.div`
   padding: ${({ theme }) => theme.spacing.units(2)};
   padding-bottom: 0;
+  padding-top: ${({ theme }) => theme.spacing.units(10)};
+
   margin: 0 ${({ theme }) => theme.spacing.units(20)};
 
   ${({ theme }) => theme.media.medium`
     margin: 0 ${theme.spacing.units(10)};
   `}
-
+  
   ${({ theme }) => theme.media.small`
-    padding: ${theme.spacing.units(1)};
-    padding-bottom: 0;
     margin: 0;
-  `}
+  `};
 `;
 
-export default function Layout({ children, title }) {
-  return (
-    <Root>
-      <Helmet title={title} />
-      <Container>
-        {children}
-      </Container>
-    </Root>
-  );
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menuOpen: false,
+    };
+  }
+
+  setMenuState(open) {
+    this.setState({ menuOpen: open });
+  }
+
+  render() {
+    const { children, title } = this.props;
+    const { menuOpen } = this.state;
+
+    return (
+      <Root>
+        <Helmet title={title} />
+        <AppBar>
+          <Toolbar disableGutters>
+            <Button
+              variant="text"
+              onClick={() => this.setMenuState(true)}
+              aria-label="Main menu"
+            >
+              <MenuIcon />
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <DrawerMenu
+          open={menuOpen}
+          onClose={() => this.setMenuState(false)}
+          onOpen={() => this.setMenuState(true)}
+        />
+        <Container>{children}</Container>
+      </Root>
+    );
+  }
 }
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
   title: PropTypes.string.isRequired,
 };
+
+export default Layout;
