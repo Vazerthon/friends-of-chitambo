d9ec84b6a046cc6621d12cebd202920f57bdab3b
Paul Vallis <vallis1@gmail.com>
2019-01-20 20:57:33 +0000

    Add social links to contacts

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { IoLogoTwitter, IoLogoFacebook } from 'react-icons/io';
 
 import Link from './link';
 import Panel from './panel';
@@ -10,6 +11,12 @@ import { ButtonLink } from './link';
 const Aside = styled.aside`
   width: 100%;
   flex: 1 0 0%;
+`;
+
+const Row = styled(Paragraph)`
+  display: flex;
+  align-items: center;
+  margin-bottom: ${({ theme }) => theme.spacing.units(1)};
 `;
 
 const SidePanel = styled(Panel)`
@@ -26,13 +33,23 @@ export default function Sidebar() {
   return (
     <Aside>
       <SidePanel title="Contact">
-        <Paragraph>
+        <Row>
           email: <Link href="mailto:info@friendsofchitambo.org.uk">info@friendsofchitambo.org.uk</Link>
-          <br />
+        </Row>
+        <Row>
           tel (UK): <Link href="tel:00441316509382">+44 (0)131 650 9382</Link>
-          <br />
+        </Row>
+        <Row>
           tel (Zambia): <Link href="tel:0026977571685">+26 (0)97 757 1685</Link>
-        </Paragraph>
+        </Row>
+        <Row>
+          <IoLogoFacebook />
+          <Link target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/friendsofchitambo/">@friendsofchitambo</Link>
+        </Row>
+        <Row>
+          <IoLogoTwitter />
+          <Link target="_blank" rel="noopener noreferrer" href="https://twitter.com/FriendsChitambo">@FriendsChitambo</Link>
+        </Row>
       </SidePanel>
       <SidePanel title="Donate">
         <Paragraph>Thank you for your support!</Paragraph>
