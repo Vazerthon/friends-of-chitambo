0ebd060362f53c7abb98330beb53e78fc04c6582
Paul Vallis <vallis1@gmail.com>
2019-02-03 22:46:02 +0000

    Start custom day renderer

--- src/components/calendar.jsx
+++ src/components/calendar.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
-import { Calendar as MaterialCalendar } from 'material-ui-pickers';
+import format from 'date-fns/format';
+import { Calendar as MaterialCalendar, Day } from 'material-ui-pickers';
+
+const events = [
+  new Date('4th Feb 2019'),
+  new Date('20th Feb 2019'),
+  new Date('8th Feb 2019'),
+  new Date('14th Feb 2019'),
+];
+
+const noop = () => {};
+const dayOfMonth = date => format(date, 'd');
+const renderDay = (day, _, isDayInCurrentMonth) => (
+  <Day
+    disabled={false}
+    current={false}
+    hidden={!isDayInCurrentMonth}
+    selected={false}
+  >
+    {dayOfMonth(day)}
+  </Day>
+);
 
 export default function Calendar() {
   return (
-    <MaterialCalendar date={new Date()} onChange={console.log} />
+    <MaterialCalendar
+      date={new Date()}
+      onChange={noop}
+      renderDay={renderDay}
+    />
   );
 }
