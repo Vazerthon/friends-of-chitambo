1f6fbc2e34f0a7a2721691e380a135328e6bb9c3
Paul Vallis <vallis1@gmail.com>
2019-02-09 11:30:24 +0000

    Set up jest/enzyme for testing

---
+++ enzyme-setup.js
@@ -0,0 +1,7 @@
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+const enzyme = require('enzyme');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const Adapter = require('enzyme-adapter-react-16');
+
+module.exports = enzyme.configure({ adapter: new Adapter() });
