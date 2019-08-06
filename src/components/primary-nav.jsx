07edaf8454944619553dac18a8bc0552cd87b142
Vallis <paul@mrvallis.co.uk>
2019-08-06 21:24:11 +0100

    Extract components

--- src/components/primary-nav.jsx
+++ src/components/primary-nav.jsx
@@ -1,34 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import { Link } from 'gatsby';
+import NavLink, { Nav } from './nav-link';
 
 const lightestFirst = (x, y) => (x.weight < y.weight ? -1 : 1);
-
-const Nav = styled.nav`
-  display: flex;
-  align-items: center;
-
-  /* styling for active NavLink */
-  .active {
-    border-bottom: 1px solid ${({ theme }) => theme.colour.neutral.black};
-  }
-`;
-
-const NavLink = styled(Link)`
-  color: ${({ theme }) => theme.colour.neutral.black};
-  text-decoration: none;
-  outline: none;
-  margin: ${({ theme }) => `0 ${theme.spacing.units(4)};`};
-
-  ${({ theme }) => theme.media.medium`margin: 0 ${theme.spacing.units(2)};`}
-  ${({ theme }) => theme.media.small`margin: 0 ${theme.spacing.units(1)};`}
-
-  :focus, :hover {
-    transform: scale(1.2);
-    font-weight: bold;
-  }
-`;
 
 function PrimaryNav({ items }) {
   return (
