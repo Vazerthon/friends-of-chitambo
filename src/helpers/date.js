4aad4e088e1c6ff975b632414af8f086f6b9016f
Paul Vallis <vallis1@gmail.com>
2020-07-14 20:51:26 +0100

    Remove date helpers

--- src/helpers/date.js
+++
@@ -1,5 +0,0 @@
-import format from 'date-fns/format';
-import parseIso from 'date-fns/parseISO';
-
-// eslint-disable-next-line import/prefer-default-export
-export const formatFullDate = dateString => format(parseIso(dateString), 'do MMMM yyyy', { awareOfUnicodeTokens: true });
