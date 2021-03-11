8bcd5fdfc32ad678b8a183f9d263af91a9a400b4
Paul Vallis <vallis1@gmail.com>
2021-03-11 11:24:58 +0000

    Remove enzyme setup

--- enzyme-setup.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-const enzyme = require('enzyme');
-// eslint-disable-next-line import/no-extraneous-dependencies
-const Adapter = require('enzyme-adapter-react-16');
-
-module.exports = enzyme.configure({ adapter: new Adapter() });
